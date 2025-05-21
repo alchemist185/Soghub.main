4731a0a60ffd06d737da07410ac44de20caf08aa
spiggity <moniummmm12345678@gmail.com>
2025-05-21 22:45:40 +0800

    Create main..lua

---
+++ main..lua
@@ -0,0 +1,79 @@
+--[[This file was protected with MoonSec V3]]):gsub('.+', (function(a) _
+repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game.Players.LocalPlayer
+
+local 𝓐 = game
+local 𝓑 = loadstring(𝓐:HttpGet(string.reverse("dleifyaR/sunem.suiriS//:sptth")))()
+
+local 𝓒 = 𝓑:CreateWindow({
+    Name = "𝒟𝒲",
+    LoadingTitle = "𝒮𝑜𝑔 𝒽𝓊𝒷",
+    LoadingSubtitle = "By Alchemist",
+    ConfigurationSaving = {
+        Enabled = true,
+        FolderName = "𝒟",
+        FileName = "𝒟"
+    },
+    Discord = {
+        Enabled = false
+    },
+    KeySystem = false
+})
+
+local 𝓜𝓪𝓲𝓷 = 𝓒:CreateTab("",4483362458)
+𝓜𝓪𝓲𝓷:CreateSection("")
+
+local 𝓐𝓲𝓶 = 𝓒:CreateTab("",4483362458)
+𝓐𝓲𝓶:CreateSection("")
+𝓐𝓲𝓶:CreateButton({
+    Name = "",
+    Callback = function()
+        loadstring(𝓐:HttpGet(string.reverse("niaM/niam.toBbmA/581tsimehcAresuhtiw/moc.tnetnocresuhtiwbuhtig//:sptth")))()
+    end,
+})
+
+local 𝓣𝓮𝓵𝓮 = 𝓒:CreateTab("",4483362458)
+𝓣𝓮𝓵𝓮:CreateSection("")
+
+local function 𝓣𝓟(𝓷,𝓾)
+    𝓣𝓮𝓵𝓮:CreateButton({
+        Name = 𝓷,
+        Callback = function()
+            𝓑:Notify({
+                Title = "",
+                Content = "",
+                Duration = 2
+            })
+            loadstring(𝓐:HttpGet(𝓾))()
+            task.wait(2)
+            𝓑:Notify({
+                Title = "",
+                Content = "",
+                Duration = 4
+            })
+        end
+    })
+end
+
+𝓣𝓟("",string.reverse("aul.gnilretS/niam/sdaeh/sfer/oi.gnitoifitcongnilrets/aatgnir//:sptth"))
+𝓣𝓟("",string.reverse("aul.alotseptoT/niam/sdaeh/sfer/oi.gol.buhtig/moc.tnetnocresuhtiw/aatgnir//:sptth"))
+𝓣𝓟("",string.reverse("AUL.NIART/niam/sdaeh/sfer/oi.gohtn.puorg/nartpwen//:sptth"))
+𝓣𝓟("",string.reverse("aul.trofpT/niam/sdaeh/sfer/oi.gohtn.buhtig/moc.tnetnocresuhtiw/aatgnir//:sptth"))
+𝓣𝓟("",string.reverse("aul.ELTSACSTAF/niam/sdaeh/sfer/oi.gohtn.buhtig/moc.tnetnocresuhtiw/aatgnir//:sptth"))
+𝓣𝓟("",string.reverse("aul.atgnir/ria.moc.tnetnocresuhtiw/buhtig//:sptth"))
+
+local 𝓥𝓲𝓼 = 𝓒:CreateTab("",4483362458)
+𝓥𝓲𝓼:CreateSection("")
+
+𝓥𝓲𝓼:CreateButton({
+    Name = "",
+    Callback = function()
+        loadstring(𝓐:HttpGet(string.reverse("aul%20niaM/sniam/581tsimehcA/ghtillluF/tnetnocresuhtiwbuhtig//:sptth")))()
+    end
+})
+
+𝓑:Notify({
+    Title = "",
+    Content = "",
+    Duration = 2.8
+})
